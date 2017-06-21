4c98e30f254bd375e7ecdcd957fbcdf159ecea8c
Marios Karamanis <marioskaramanis1993@gmail.com>
2017-06-21 15:26:01 +0200

    lol

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+
+# coding: utf-8
+
+# In[1]:
+
 import matplotlib.plot as plt
 import numpy as np
+
+
+# In[2]:
 
 class Car(object):
 
@@ -12,7 +20,7 @@ class Car(object):
        self.vis = plt.plot(0,0, 'ro', markersize=4)[0]
 
     def accelerationRule(self):
-        if self.speed <5:
+        if self.speed < maxSpeed:
            self.speed+=1
 
     def getx(self):
@@ -21,7 +29,7 @@ class Car(object):
     def getEdge(self):
         return self.edge
 
-    def RandomizationRule(self):
+    def randomizationRule(self):
         var=np.random.uniform(0,1)
         if self.speed >=1:
              if var<=0.5:
@@ -33,16 +41,15 @@ class Car(object):
             self.edge = route[0]
         else: self.edge = False
 
-    def setLocation(self, speed):
-        newX = x + speed*timestep
+    def updateLocation(self):
+        newX = self.x + self.speed*timestep
         # Updating visualisation information
         self.vis.set_data(newX,0)
         # Updating location
         self.x = newX
 
     def distanceRule(self,frontCar):
-
-        difference=frontcar.getx()-self.x
+        difference=frontCar.getx()-self.x
         if difference<self.speed:
            self.speed=difference
 
@@ -54,47 +61,58 @@ class Edge(object):
         plt.plot((x1,x2),(y1,y2))
         self.node1 = node1
         self.node2 = node2
-        self.length = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+        self.length = np.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 totCars = 10
 influx = 1 #cars per timestep
-timestep = 1 #minutes per timestep
-network = [Edge((0,0),())]
+timestep = 1 #seconds per timestep
+time = 100
+maxSpeed = 5
 
 def addNewCars(influx):
     newCars = []
     for i in influx:
         numEdges = random([1,10])
         startEdge = random(network)
-        route = [startEdge, …, …, …]
+        route = [startEdge]
         car = Car(route)
         newCars.append(car)
     return newCars
 
+
+# In[3]:
+
 if __name__ == '__main__':
-	# Activate interactive visualisation
-	plt.ion()
-	# Initialise network, for now only one road
-	road = Edge((0,0),(100,0))
+# Activate interactive visualisation
+    plt.ion()
+# Initialise network, for now only one road
+    road = Edge((0,0),(100,0))
 
-	# Generate all agents
-	carlist = []
-	for c in range(totCars):
-		pass
+    # Generate all agents
+    cars = []
+    for c in range(totCars):
+        pass
 
-	for t in time:
-		cars += addNewCars(influx)
-		newCarList = []
-		for n, car in enumerate(cars):
-			myLocation = car.getLocation()
-			hisLocation = cars[n+1].getLocation()
-			distance =hisLocation - myLocation
-			acceleration = accelerationRule(distance)
-			car.setLocation() = computeNewLocation(acceleration, car)
-			if not car.getLocation() == offMap:
-				newCarList.append(car)
+    for t in range(time):
+        cars.append(Car(road, 0, [road], maxSpeed))
+            
+        for n, car in enumerate(cars):
+            car.accelerationRule()
+            car.randomizationRule()
+            if n != 0:
+                frontCar = cars[n-1]
+                car.distanceRule(frontCar)
+            car.updateLocation()
+#             if not car.getLocation() == offMap:
+#                 newCarList.append(car)
 
-    	plt.pause(0.1)
+        plt.pause(0.1)
 
 
-	averageSpeed = sum([car.getAverageSpeed() for car in cars])/len(cars)
+    averageSpeed = sum([car.getAverageSpeed() for car in cars])/len(cars)
+
+
+# In[11]:
+
+
+
