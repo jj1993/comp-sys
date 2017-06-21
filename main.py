e7423f72ed9c6b2804b91f098bb3f826d8738fa2
Jonathan Jeroen Beekman <beekm@wcw-stud-145-109-2-59.wireless.uva.nl>
2017-06-21 17:59:00 +0200

    Network version

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
-
-# coding: utf-8
-
-# In[1]:
-
-import matplotlib.plot as plt
+import matplotlib.pyplot as plt
 import numpy as np
-
-
-# In[2]:
+import random
 
 class Car(object):
 
-    def __init__(self, edge, x, route,speed):
+    def __init__(self, edge, route, speed):
        self.edge = edge
        self.route = route
-       self.x = x
+       self.pos = 0
        self.speed=speed
        # Visualisation point
-       self.vis = plt.plot(0,0, 'ro', markersize=4)[0]
+       x, y = edge.getStart()
+       self.vis = plt.plot(x, y, 's', markersize=4)[0]
 
-    def accelerationRule(self):
-        if self.speed < maxSpeed:
-           self.speed+=1
-
-    def getx(self):
-        return self.x
+    def getPos(self):
+        return self.pos
 
     def getEdge(self):
         return self.edge
 
-    def randomizationRule(self):
-        var=np.random.uniform(0,1)
-        if self.speed >=1:
-             if var<=0.5:
-                 self.speed-=1
+# The three speed rules
+    def accelerationRule(self):
+        if self.speed < maxSpeed - 1:
+           self.speed+=1
 
+    def randomizationRule(self):
+        if self.speed > 1:
+             if np.random.uniform(0,1) < p:
+                 self.speed -= 1
+
+    def distanceRule(self, frontCar):
+        hisEdge = frontCar.getEdge()
+        if hisEdge == self.edge:
+            difference = frontCar.getPos() - self.pos
+            print('-  ',round(difference))
+        else:
+            difference = (
+                 self.edge.getLength() - self.pos
+                 + frontCar.getPos()
+                 )
+            print('+  ',round(difference))
+
+        difference = abs(difference)
+        if difference + 2 < self.speed:
+            self.speed=difference
+
+# Updating locations and moving to new edges
     def setNewEdge(self):
         del self.route[0]
         if len(self.route) != 0:
@@ -42,16 +53,43 @@ class Car(object):
         else: self.edge = False
 
     def updateLocation(self):
-        newX = self.x + self.speed*timestep
-        # Updating visualisation information
-        self.vis.set_data(newX,0)
-        # Updating location
-        self.x = newX
+        distance = self.speed*timestep
+        if self.pos + distance < self.edge.getLength():
+            # Updating visualisation information
+            self.pos += distance
+            x, y = self.edge.getXY(self.pos)
+            self.vis.set_data(x, y)
+            return True
+        else:
+            if len(self.route) == 0:
+                self.vis.remove()
+                self.pos += distance
+                self.edge.removeCar(self)
+                return False
+            else:
+                distance -= self.edge.getLength() - self.pos
+                self.pos = distance
+                self.edge.removeCar(self)
+                self.edge = self.route[0]
+                self.edge.addCar(self)
+                del self.route[0]
+                x, y = self.edge.getXY(self.pos)
+                self.vis.set_data(x, y)
+                return True
 
-    def distanceRule(self,frontCar):
-        difference=frontCar.getx()-self.x
-        if difference<self.speed:
-           self.speed=difference
+    def getFrontCar(self):
+        for car in self.edge.getCars():
+            if car.getPos() > self.pos:
+                return car
+        if len(route) != 0:
+            nextCarList = route[0].getCars()
+            if len(nextCarList) != 0:
+                print("CAR ON DIFFERENT EDGE")
+                return nextCarList[-1]
+        return False
+
+    def activate(self):
+        self.edge.addCar(self)
 
 
 class Edge(object):
@@ -59,60 +97,85 @@ class Edge(object):
         x1, y1 = node1
         x2, y2 = node2
         plt.plot((x1,x2),(y1,y2))
-        self.node1 = node1
-        self.node2 = node2
         self.length = np.sqrt((x2-x1)**2 + (y2-y1)**2)
+        self.angle = np.arctan((y2-y1)/(x2-x1))
+        self.x1, self.y1 = x1, y1
+        self.x2, self.y2 = x2, y2
+        self.cars = []
 
+    def getLength(self):
+        return self.length
+
+    def getStart(self):
+        return (self.x1, self.y1)
+
+    def getXY(self, pos):
+        x = self.x1 + np.cos(self.angle)*pos
+        y = self.y1 + np.sin(self.angle)*pos
+        return (x, y)
+
+    def addCar(self, car):
+        self.cars.append(car)
+
+    def removeCar(self, car):
+        for n, myCar in enumerate(self.cars):
+            if myCar == car:
+                del self.cars[n]
+                return
+
+    def getCars(self):
+        return self.cars
+
+# Some constants
 totCars = 10
 influx = 1 #cars per timestep
 timestep = 1 #seconds per timestep
 time = 100
 maxSpeed = 5
-
-def addNewCars(influx):
-    newCars = []
-    for i in influx:
-        numEdges = random([1,10])
-        startEdge = random(network)
-        route = [startEdge]
-        car = Car(route)
-        newCars.append(car)
-    return newCars
-
-
-# In[3]:
+p = 0.5
 
 if __name__ == '__main__':
-# Activate interactive visualisation
+    # Activate interactive visualisation
     plt.ion()
-# Initialise network, for now only one road
-    road = Edge((0,0),(100,0))
+    # Initialise network, for now only one road
+    road1 = Edge((0,0),(100,0))
+    road2 = Edge((100,0),(200,-10))
+    road3 = Edge((100,0),(200,10))
 
     # Generate all agents
     cars = []
+    activeCars = []
     for c in range(totCars):
-        pass
+        road = random.choice([road2, road3])
+        route = [road1, road]
+        speed = maxSpeed - 2 + 2*np.random.random()
+        cars.append(Car(route[0], route[1:], speed))
 
     for t in range(time):
-        cars.append(Car(road, 0, [road], maxSpeed))
-            
-        for n, car in enumerate(cars):
+        if t%1 == 0 and len(cars) != 0:
+            cars[0].activate()
+            activeCars.append(cars[0])
+            del cars[0]
+
+        newActiveCars = []
+        for n, car in enumerate(activeCars):
+            # if not (n == 0 and not (t < 10 or t > 25)):
             car.accelerationRule()
             car.randomizationRule()
             if n != 0:
-                frontCar = cars[n-1]
-                car.distanceRule(frontCar)
-            car.updateLocation()
-#             if not car.getLocation() == offMap:
-#                 newCarList.append(car)
+                frontCar = car.getFrontCar()
+                if frontCar:
+                    car.distanceRule(frontCar)
 
-        plt.pause(0.1)
+        for car in activeCars:
+            onEdge = car.updateLocation()
+            if onEdge:
+                newActiveCars.append(car)
 
+        print([round(car.getPos()) for car in newActiveCars])
+        activeCars = newActiveCars
+        plt.pause(0.5)
 
     averageSpeed = sum([car.getAverageSpeed() for car in cars])/len(cars)
 
-
 # In[11]:
-
-
-
