39da1827e009a6450f396131d862b31e28750904
Marios Karamanis <marioskaramanis1993@gmail.com>
2017-06-21 14:17:46 +0200

    marios

--- main.py
+++ main.py
@@ -2,16 +2,30 @@ import matplotlib.plot as plt
 import numpy as np
 
 class Car(object):
-	def __init__(self, edge, x, route,speed):
-	   self.edge = edge
+
+    def __init__(self, edge, x, route,speed):
+       self.edge = edge
        self.route = route
-	   self.x = x
-	   self.speed=speed
-	   # Visualisation point
-	   self.vis = plt.plot(0,0, 'ro', markersize=4)[0]
+       self.x = x
+       self.speed=speed
+       # Visualisation point
+       self.vis = plt.plot(0,0, 'ro', markersize=4)[0]
+
+    def accelerationRule(self):
+        if self.speed <5:
+           self.speed+=1
+
+    def getx(self):
+        return self.x
 
     def getEdge(self):
         return self.edge
+
+    def RandomizationRule(self):
+        var=np.random.uniform(0,1)
+        if self.speed >=1:
+             if var<=0.5:
+                 self.speed-=1
 
     def setNewEdge(self):
         del self.route[0]
@@ -21,19 +35,26 @@ class Car(object):
 
     def setLocation(self, speed):
         newX = x + speed*timestep
-		# Updating visualisation information
-		self.vis.set_data(newX,0)
-		# Updating location
-		self.x = newX
+        # Updating visualisation information
+        self.vis.set_data(newX,0)
+        # Updating location
+        self.x = newX
+
+    def distanceRule(self,frontCar):
+
+        difference=frontcar.getx()-self.x
+        if difference<self.speed:
+           self.speed=difference
+
 
 class Edge(object):
-	def __init__(self, node1, node2):
-		x1, y1 = node1
-		x2, y2 = node2
-		plt.plot((x1,x2),(y1,y2))
-		self.node1 = node1
-		self.node2 = node2
-		self.length = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    def __init__(self, node1, node2):
+        x1, y1 = node1
+        x2, y2 = node2
+        plt.plot((x1,x2),(y1,y2))
+        self.node1 = node1
+        self.node2 = node2
+        self.length = math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 totCars = 10
 influx = 1 #cars per timestep
@@ -41,34 +62,14 @@ timestep = 1 #minutes per timestep
 network = [Edge((0,0),())]
 
 def addNewCars(influx):
-	newCars = []
-	for i in influx:
-		numEdges = random([1,10])
-		startEdge = random(network)
-		route = [startEdge, …, …, …]
-		car = Car(route)
-		newCars.append(car)
-	return newCars
-
-def accelerationRule(distance,speed):
-	# Nagel-Schreckenberg
-	safedist=2*speed
-	if distance<safedist:
-		return distance/safedist
-	else
-		return 1
-def distance(car1,car2):
-	return abs(car1.x-car2.X)
-
-def newspeed(speed):
-	return speed*acceleration
-
-def computeNewLocation(acceleration, car):
-	distance = acceleration*timestep
-	thisEdge = car.getEdge()
-	if thisEdge.length() - car.getLocation() < distance:
-		car.setEdge(car.getNewEdge())
-	car.setNewLocation()
+    newCars = []
+    for i in influx:
+        numEdges = random([1,10])
+        startEdge = random(network)
+        route = [startEdge, …, …, …]
+        car = Car(route)
+        newCars.append(car)
+    return newCars
 
 if __name__ == '__main__':
 	# Activate interactive visualisation
@@ -92,7 +93,7 @@ if __name__ == '__main__':
 			car.setLocation() = computeNewLocation(acceleration, car)
 			if not car.getLocation() == offMap:
 				newCarList.append(car)
-			
+
     	plt.pause(0.1)
 
 
